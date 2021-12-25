ff4cda25fc5c9df9d1065f043e34f77b73b18cd9
Ravilochan <ravilochan2000@gmail.com>
2021-12-25 22:06:55 +0530

    mogran & Express added

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,23 @@
 import "reflect-metadata";
+import express from "express";
+import morgan from "morgan";
 import { createConnection } from "typeorm";
-import { User } from "./entities/User";
+// import { User } from "./entities/User";
+const app = express();
 
-createConnection()
-  .then(async (connection) => {
-    console.log("Inserting a new user into the database...");
-    const user = new User();
-    user.firstName = "Timber";
-    user.lastName = "Saw";
-    user.age = 25;
-    await connection.manager.save(user);
-    console.log("Saved a new user with id: " + user.id);
+app.use(express.json());
+app.use(morgan("dev"));
 
-    console.log("Loading users from the database...");
-    const users = await connection.manager.find(User);
-    console.log("Loaded users: ", users);
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
 
-    console.log("Here you can setup and run express/koa/any other framework.");
-  })
-  .catch((error) => console.log(error));
+app.listen(4000, async () => {
+  try {
+    console.log("Server started on port 4000");
+    await createConnection();
+    console.log("Connected to database");
+  } catch (error) {
+    console.error(error);
+  }
+});
